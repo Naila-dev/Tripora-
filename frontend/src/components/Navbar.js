c4a4ec7d02f483ee2695867bb436da739d684a21
Naila-dev <naila.njambi.dev@gmail.com>
2025-11-04 16:31:44 +0300

    added build

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 import '../styles/navbar.css'; // Import the new CSS file
 
 export default function Navbar({ onLoginClick, onRegisterClick }) {
+  const { user, logout } = useContext(AuthContext);
+  const firstName = user?.name?.split(' ')[0];
+
   return (
     <nav className="navbar-container">
       {/* Left Side */}
@@ -15,16 +19,23 @@ export default function Navbar({ onLoginClick, onRegisterClick }) {
       <div className="navbar-right">
         <ul className="navbar-links">
           <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>cd ba
+          <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/tours-list">Tours</NavLink></li>
           <li><NavLink to="/blog">Blog</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
 
-        <div className="navbar-auth">
-          <button onClick={onLoginClick} className="btn btn-outline-success">Login</button>
-          <button onClick={onRegisterClick} className="btn btn-success">Sign Up</button>
-        </div>
+        {user ? (
+          <div className="navbar-auth">
+            <span className="navbar-user-greeting">Hi, {firstName}</span>
+            <button onClick={logout} className="btn btn-outline-success">Logout</button>
+          </div>
+        ) : (
+          <div className="navbar-auth">
+            <button onClick={onLoginClick} className="btn btn-outline-success">Login</button>
+            <button onClick={onRegisterClick} className="btn btn-success">Sign Up</button>
+          </div>
+        )}
       </div>
     </nav>
   );
